2556d70d61cfb7a88a9ad2a93f071e6f288bce8e
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-04-13 17:58:55 +0900

    Update FirebaseTodo app theme

---
+++ app/src/main/java/com/eotw95/firebasetodo/FirebaseTodoApp.kt
@@ -0,0 +1,9 @@
+package com.eotw95.firebasetodo
+
+import androidx.compose.runtime.Composable
+import com.eotw95.firebasetodo.ui.theme.FirebaseTodoTheme
+
+@Composable
+fun FirebaseTodoApp() {
+    FirebaseTodoTheme {}
+}
