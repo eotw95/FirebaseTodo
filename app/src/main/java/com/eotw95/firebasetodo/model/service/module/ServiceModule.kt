d7134062b3796e54700340deffe1916a38db646a
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-04-13 17:27:57 +0900

    Add provideStorageService

--- app/src/main/java/com/eotw95/firebasetodo/model/service/module/ServiceModule.kt
+++ app/src/main/java/com/eotw95/firebasetodo/model/service/module/ServiceModule.kt
@@ -1,7 +1,9 @@
 package com.eotw95.firebasetodo.model.service.module
 
 import com.eotw95.firebasetodo.model.service.AccountService
+import com.eotw95.firebasetodo.model.service.StorageService
 import com.eotw95.firebasetodo.model.service.impl.AccountServiceImpl
+import com.eotw95.firebasetodo.model.service.impl.StorageServiceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,4 +13,5 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 abstract class ServiceModule {
     @Binds abstract fun provideAccountService(impl: AccountServiceImpl): AccountService
+    @Binds abstract fun provideStorageService(impl: StorageServiceImpl): StorageService
 }
