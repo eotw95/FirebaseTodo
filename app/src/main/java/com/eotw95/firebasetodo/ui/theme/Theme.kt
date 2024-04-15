a3fe5022c974cea2d9a21aef9849d415e72b8c5d
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-04-15 13:10:57 +0900

    Update API calls for compose.material library migration

--- app/src/main/java/com/eotw95/firebasetodo/ui/theme/Theme.kt
+++ app/src/main/java/com/eotw95/firebasetodo/ui/theme/Theme.kt
@@ -1,21 +1,21 @@
 package com.eotw95.firebasetodo.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorScheme = darkColorScheme(
+private val DarkColorPalette = darkColors(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    primaryVariant = Pink40
 )
 
-private val LightColorScheme = lightColorScheme(
+private val LightColorPalette = lightColors(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    primaryVariant = Pink40
 )
 
 @Composable
@@ -23,9 +23,9 @@ fun FirebaseTodoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
     MaterialTheme(
-        colorScheme = colorScheme,
+        colors = colors,
         typography = Typography,
         content = content
     )
