30bb68b4387851929aa6712d1722a2041feefcb0
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-04-17 00:03:15 +0900

    Add TaskItem Composable

--- app/src/main/java/com/eotw95/firebasetodo/screens/tasks/TaskItem.kt
+++ app/src/main/java/com/eotw95/firebasetodo/screens/tasks/TaskItem.kt
@@ -1,6 +1,70 @@
 package com.eotw95.firebasetodo.screens.tasks
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AssistantPhoto
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eotw95.firebasetodo.model.Task
 
 @Composable
-fun TaskItem() {}
+fun TaskItem(
+    task: Task,
+    options: List<String>,
+    onCheckChange: () -> Unit,
+    onActionClick: (String) -> Unit
+) {
+    Card(
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = Modifier.padding(8.dp, 0.dp, 8.dp, 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Checkbox(
+                checked = task.completed,
+                onCheckedChange = { onCheckChange() },
+                modifier = Modifier.padding(8.dp, 0.dp)
+            )
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = task.title, style = MaterialTheme.typography.subtitle2)
+                // Todo: 引数の文法がよくわからん
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(text = formatDateAndTime(task), fontSize = 12.sp)
+                }
+            }
+            if (task.flag) {
+                Icon(imageVector = Icons.Filled.AssistantPhoto, contentDescription = "Flag")
+            }
+            // Todo: DropDownMenu
+        }
+    }
+}
+
+private fun formatDateAndTime(task: Task): String {
+    val strBuilder = StringBuilder("")
+    if (task.dueDate.isNotBlank()) {
+        strBuilder.append(task.dueDate)
+        strBuilder.append(" ")
+    }
+    if (task.dueTime.isNotBlank()) {
+        strBuilder.append("at ")
+        strBuilder.append(task.dueTime)
+    }
+    return strBuilder.toString()
+}
